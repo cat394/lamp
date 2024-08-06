763acc1d1b72ef7d0a220f40c0fffa101c7fa36f
cat394 <keyring1231@gmail.com>
2024-08-06 21:10:55 +0900

    Remove mongo file and Remove unnecessary dependency
    - I deleted the MongoDB related files that I had created as a test. I moved them to @kokomi/mongo-request-handler.
    
    - I removed lodash from my deno.json file as it was an unnecessary dependency.

--- mod.ts
+++ mod.ts
@@ -1,15 +1,3 @@
 import { html } from "./html.ts";
-import type { DatabaseConfig, Document, Query , BasicEndpoints } from "./mongo/types.ts";
-import type * as RequestResult from "./mongo/result-types.ts";
-import { createMongoDBClient, MongoDBRequestError } from "./mongo/main.ts";
 
-export {
-  createMongoDBClient,
-  type DatabaseConfig,
-  type Document,
-	type BasicEndpoints,
-  html,
-  MongoDBRequestError,
-  type Query,
-  type RequestResult,
-};
+export { html };
